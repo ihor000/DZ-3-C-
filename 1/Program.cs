7e15f4d9a24a8cad5d15944550d8e8723b18c5ee
ihor000 <ihor.silnov@yandex.ru>
2022-12-09 19:14:32 +0300

    text

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите пятизначное число:");
+int number = int.Parse(Console.ReadLine()!);
+
+
+CheckPalindromicNumber(number);
+ void CheckPalindromicNumber(int number)
+
+{
+    if(number >= 10000)
+    {
+        int division1 = number / 10000;
+        int remainder1 = number % 10;
+ 
+            if(division1 == remainder1)
+            {
+                number = number / 10;
+                int division2 = (number / 100) % 10;
+                int remainder2 = number % 10;
+                if(division2 == remainder2)
+                    Console.WriteLine("Палиндром");
+            }
+            else 
+            Console.WriteLine("Не палиндром");
+            
+    }
+    else
+    Console.WriteLine("Введино неверное число!");
+}
+ 
