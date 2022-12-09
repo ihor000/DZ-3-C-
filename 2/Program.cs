33833d4ae8de53a61226fd37bbfaa9183fec9f94
ihor000 <ihor.silnov@yandex.ru>
2022-12-10 01:44:10 +0300

    text

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите координату первой точки X");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату первой точки Y");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату первой точки Z");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату второй точки X");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату второй точки Y");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату второй точки Z");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+int A = x1 - x2;
+int B = y1 - y2;
+int C = z1 - z2;
+
+
+double length = Math.Sqrt(A * A + B * B + C * C);
+
+Console.WriteLine($"Длинна отрезка {length}");
