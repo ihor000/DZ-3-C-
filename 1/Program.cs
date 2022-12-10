b7f572bc6f29aac5a8e49b20ed51ba126db082a8
ihor000 <ihor.silnov@yandex.ru>
2022-12-10 22:09:49 +0300

    text

--- 1/Program.cs
+++ 1/Program.cs
@@ -2,27 +2,27 @@
 int number = Convert.ToInt32(Console.ReadLine());
 
 
-    if(number >= 10000)
-    {
-        int division1 = number / 10000;
-        int remainder1 = number % 10;
+if(number >= 10000)
+{
+    int division1 = number / 10000;
+    int remainder1 = number % 10;
  
-            if(division1 == remainder1)
-            {
-                number = number / 10;
-                int division2 = (number / 100) % 10;
-                int remainder2 = number % 10;
-                if(division2 == remainder2)
-                    Console.WriteLine("Палиндром");
-            }
-            else 
-            Console.WriteLine("Не палиндром");
+if(division1 == remainder1)
+{
+    number = number / 10;
+    int division2 = (number / 100) % 10;
+    int remainder2 = number % 10;
+    if(division2 == remainder2)
+    Console.WriteLine("Палиндром");
+}
+else 
+    Console.WriteLine("Не палиндром");
             
-    }
-    else
-    {
-        Console.WriteLine("Введино неверное число!");
-    }
+}
+else
+{
+    Console.WriteLine("Введино неверное число!");
+}
    
 
  
