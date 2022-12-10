dea081b7f6f916ef49ec6b5010605661a06ebb06
ihor000 <ihor.silnov@yandex.ru>
2022-12-10 15:38:46 +0300

    text

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Введите число");
+﻿Console.WriteLine("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+{ 
+    Console.Write($"{i*i*i} ");
+}
+
+
+
+// Функция ввода сообщения
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
